53c0c16d48d7735a7a0beceae1911290e0044274
francoriquelme <francoestebanru@gmail.com>
2022-05-12 21:56:32 -0400

    First Commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,41 @@
+class PostsController < ApplicationController
+    def index
+        @posts = Post.all
+    end
+
+    def show
+        @post = Post.find(params[:id])
+    end
+
+    def new
+        @post = Post.new
+    end 
+    
+    def edit
+        @post = Post.find(params[:id])
+    end
+    
+    def create
+        @post = Post.new(params)
+        if @post.save
+            redirect_to @post, notice: "Post was successfully created."
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        if @post.update(params)
+            redirect_to @post, notice: "Post was successfully updated."
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+    
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to posts_url, notice: "Post was successfully destroyed."
+    end
+end
+    
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  # resources :posts
+  # resources :posts, only: [:index, :show, :create, :update, :destroy]
+  resources :posts, except: [:new, :edit]
+
+end
