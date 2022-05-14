ed88dec56b1b598d0bb6da2250a8f566d212950d
francoriquelme <francoestebanru@gmail.com>
2022-05-13 20:54:52 -0400

    Post was successfully created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,26 @@
 class PostsController < ApplicationController
+    before_action :set_post, only: %i[ show edit update destroy ]
     def index
         @posts = Post.all
     end
-
     def show
         @post = Post.find(params[:id])
     end
-
     def new
         @post = Post.new
     end
-
     def create
-        @post = Post.new(params)
-        if @post.save
-            redirect_to @post, notice: "Post was successfully created."
-        else
-            render :new, status: :unprocessable_entity
+        @post = Post.new(post_params)
+        respond_to do |format|
+          if @post.save
+            format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
+            format.json { render :show, status: :created, location: @post }
+          else
+            format.html { render :new, status: :unprocessable_entity }
+            format.json { render json: @post.errors, status: :unprocessable_entity }
+          end
         end
     end
-
     def update
         if @post.update(params)
             redirect_to @post, notice: "Post was successfully updated."
@@ -27,11 +28,18 @@ class PostsController < ApplicationController
             render :edit, status: :unprocessable_entity
         end
     end
-
     def destroy
         @post = Post.find(params[:id])
         @post.destroy
         redirect_to posts_url, notice: "Post was successfully destroyed."
-    end   
-end   
-    
+    end
+    private
+        # Use callbacks to share common setup or constraints between actions.
+        def set_post
+            @post = Post.find(params[:id])
+        end
+        # Only allow a list of trusted parameters through.
+        def post_params
+            params.require(:post).permit(:title, :description, :author)
+        end
+end
